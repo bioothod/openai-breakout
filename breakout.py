abd4d14eee657aa3bbadb0fd9b99dfc09fc66c03
Evgeniy Polyakov <zbr@ioremap.net>
2017-07-19 23:30:07 +0300

    breakout: increase batch size to 512

--- breakout.py
+++ breakout.py
@@ -10,7 +10,7 @@ class breakout(object):
         c.put('gradient_update_step', 40000)
         c.put('gamma', 0.99)
         c.put('update_reward_steps', 10)
-        c.put('batch_size', 128)
+        c.put('batch_size', 512)
         c.put('input_shape', (80, 80, 1))
         c.put('state_steps', 2)
         c.put('history_size', 100)
