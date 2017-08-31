a362a2f4c101e00daa30ba13b0162de840daf7e9
Evgeniy Polyakov <zbr@ioremap.net>
2017-08-31 18:15:44 +0100

    Reduce default gradient clipping to 0.1 which shows better performace

--- breakout.py
+++ breakout.py
@@ -37,7 +37,7 @@ class breakout(object):
         c.put('import_self_weight', 0.1)
 
         c.put('reward_mean_alpha', 0.9)
-        c.put('clip_gradient_norm', 1.)
+        c.put('clip_gradient_norm', 0.1)
         c.put('xentropy_reg_beta', 0.01)
         #c.put('policy_reg_beta', 0.01)
 
